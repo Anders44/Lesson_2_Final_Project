08f2388b11a642313b70ab9da87e980cc580beaa
Unknown <anderskornkven@gmail.com>
2018-06-03 18:43:00 -0600

    Working to add repos into webpage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let gitHubRequest = new XMLHttpRequest();
 gitHubRequest.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     let gitObject = JSON.parse(this.responseText);
-    document.getElementById("gitRepos").innerHTML = gitObject.repos_url;
+    document.getElementById("gitBio").innerHTML = gitObject.bio;
   }
 };
 gitHubRequest.open("GET", "https://api.github.com/users/Anders44", true);
